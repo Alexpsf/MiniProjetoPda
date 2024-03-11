635ca6ce2399902eecaaac133799ae85f3ab3be1
Alex <Alexsandrosantanafilho@gmail.com>
2024-03-11 10:00:34 -0300

    Ajustes na lógica do Js, agora ele conta as tarefas, mudança na estilização do Css adotando uma paleta de cor primaveril e adição de imagens para compor a estilização, adição de um título H1 no Html com o nome do projeto.

--- Algo.js
+++ Algo.js
@@ -1,76 +1,54 @@
 let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
 let btnNovaTarefa = document.querySelector('#btnAddTarefa');
 let listaTarefas = document.querySelector('#listaTarefas');
+let contadorTarefas = 0; 
 
 inputNovaTarefa.addEventListener('keypress', (e) => {
-
-    if(e.keyCode == 13) {
-        let tarefa = {
-            nome: inputNovaTarefa.value, id: gerarId(), 
-
-        }
-            adicionarTarefa(tarefa);
+    if (e.keyCode == 13) {
+        adicionarTarefa(inputNovaTarefa.value);
     }
-
 });
 
-btnNovaTarefa.addEventListener('click', (e) => {
-    let tarefa = {
-        nome: inputNovaTarefa.value, id: gerarId(), 
-
-    }
-        adicionarTarefa(tarefa);
-
+btnNovaTarefa.addEventListener('click', () => {
+    adicionarTarefa(inputNovaTarefa.value);
 });
 
-
-
-function gerarId() {
-        return Math.floor(Math.random() * 3000);
-
-}
-
-function adicionarTarefa(tarefa) {
-
-    let li = criarTagLI(tarefa);
-    listaTarefas.appendChild(li);
-    inputNovaTarefa.value ='';
-
+function adicionarTarefa(nomeTarefa) {
+    if (nomeTarefa.trim() !== '') {
+        const tarefa = { id: contadorTarefas++, nome: nomeTarefa };
+        const li = criarTagLI(tarefa);
+        listaTarefas.appendChild(li);
+        inputNovaTarefa.value = '';
+    }
 }
 
 function criarTagLI(tarefa) {
-
-    let li = document.createElement('li');
-
-    let span = document.createElement('span');
-    span.classList.add('textoTarefa');
-    span.innerHTML = tarefa.nome;
-
-    let div = document.createElement('div');
-
-    let btnEditar = document.createElement('button');
-    btnEditar.classList.add('btnAção');
-    btnEditar.innerHTML = '<i class="fa fa-pencil"></i>';
-    btnEditar.setAttribute('onclick','editar('+tarefa.id+')');
-
-
-    let btnExcluir = document.createElement('button')
-    btnExcluir.classList.add('btnAção');
-    btnExcluir.innerHTML = '<i class="fa fa-trash"></i>';
-    btnExcluir.setAttribute('onclick','excluir('+tarefa.id+')');
-
-    div.appendChild(btnEditar);
-    div.appendChild(btnExcluir);
-
-    li.appendChild(span);
-    li.appendChild(div);
+    const li = document.createElement('li');
+    li.dataset.id = tarefa.id;
+    li.innerHTML = `
+        <span class="textoTarefa">${tarefa.nome}</span>
+        <div>
+            <button class="btnAção" onclick="editar(${tarefa.id})"><i class="fa fa-pencil"></i></button>
+            <button class="btnAção" onclick="excluir(${tarefa.id})"><i class="fa fa-trash"></i></button>
+        </div>
+    `;
     return li;
 }
 
 function editar(idTarefa) {
-    alert(idTarefa);
+    const li = document.querySelector(`li[data-id="${idTarefa}"]`);
+    const textoTarefa = li.querySelector('.textoTarefa');
+    const novoNome = prompt('Opa,escreveu algo de errado? Digite o novo nome da tarefa:', textoTarefa.innerText);
+    if (novoNome !== null && novoNome.trim() !== '') {
+        textoTarefa.innerText = novoNome;
+    }
 }
 
 function excluir(idTarefa) {
-    alert(idTarefa);
+    const confirmacao = confirm('Por favor, não me exclua eu sou apenas uma tarefinha!');
+    if (confirmacao) {
+        const li = document.querySelector(`li[data-id="${idTarefa}"]`);
+        li.remove();
+    }
 }
+
